42b82dad80668f3b28621e375d00e8f1dcaa25c0
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-07-03 11:13:12 +0200

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,8 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["nb", "nn", "en"],
     routing: {
-      prefixDefaultLocale: true
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true
     }
   },
 });
